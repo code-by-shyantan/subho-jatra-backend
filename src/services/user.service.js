d5de453f813d8f98ee49953ffcad976333151bb8
SHYANTAN SAHA <shyantansaha@hotmail.com>
2026-01-06 01:25:52 +0530

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,4 @@
-const pool = require('../database/db');
+const pool = require('../../database/db');
 
 exports.findByUsername = async (username) => {
   const result = await pool.query(
@@ -21,3 +21,4 @@ exports.updateManagerPermissions = async (username, permissions) => {
     [permissions, username, 'MANAGER']
   );
 };
+
